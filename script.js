9854139d75f6ae98cb35cd0bf7b1d3b6b417e4c7
Nestor Pineda <nestorpinedaelvira@gmail.com>
2021-11-01 11:48:32 +0100

    Iteration #6

--- script.js
+++ script.js
@@ -228,3 +228,41 @@ const fusionAlienMutation = {
 };
 
 console.log(fusionAlienMutation);
+
+// #7: Reduce
+// 7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de los alumnos usando la función .reduce().
+
+const exams = [
+  { name: "Yuyu Cabeza Crack", score: 5 },
+  { name: "Maria Aranda Jimenez", score: 1 },
+  { name: "Cristóbal Martínez Lorenzo", score: 6 },
+  { name: "Mercedez Regrera Brito", score: 7 },
+  { name: "Pamela Anderson", score: 3 },
+  { name: "Enrique Perez Lijó", score: 6 },
+  { name: "Pedro Benitez Pacheco", score: 8 },
+  { name: "Ayumi Hamasaki", score: 4 },
+  { name: "Robert Kiyosaki", score: 2 },
+  { name: "Keanu Reeves", score: 10 },
+];
+
+let sumaNotas = exams.reduce((total, alumno) => {
+  return total + alumno.score;
+}, 0);
+
+// 7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los alumnos que esten aprobados usando la función .reduce().
+
+const examenesAprobados = exams.filter((notas) => {
+  return notas.score >= 5;
+});
+
+const sumaNotasExamenesAprobados = examenesAprobados.reduce((total, alumno) => {
+  return total + alumno.score;
+}, 0);
+
+console.log(sumaNotasExamenesAprobados);
+
+// 7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce().
+
+let mediaNotas = sumaNotas / exams.length;
+
+console.log(mediaNotas);
